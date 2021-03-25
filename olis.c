ef54fbca9fc674afc4fa0cb5a4358c74075ada00
Tu nombre <julicmrgo@gmail.com>
2021-03-25 20:04:20 -0300

    Nueva funcionalidad

---
+++ olis.c
@@ -0,0 +1,60 @@
+[1mdiff --git a/Alice.txt b/Alice.txt[m
+[1mnew file mode 100644[m
+[1mindex 0000000..1f2d9f8[m
+[1m--- /dev/null[m
+[1m+++ b/Alice.txt[m
+[36m@@ -0,0 +1 @@[m
+[32m+[m[32mNuevo archivo para el Bob esponja creo[m
+[1mdiff --git a/Bob.txt b/Bob.txt[m
+[1mindex caae574..a625d47 100644[m
+[1m--- a/Bob.txt[m
+[1m+++ b/Bob.txt[m
+[36m@@ -2,4 +2,6 @@[m [mEsta es una prueba de git[m
+ [m
+ Olis[m
+ [m
+[31m-pd: Bob es ponja we[m
+[32m+[m[32mpd: Bob es ponja w[m[41m [m
+[32m+[m
+[32m+[m[32mLa concha de tu madre All boys[m
+[1mdiff --git a/hola.c b/hola.c[m
+[1mnew file mode 100644[m
+[1mindex 0000000..406b5b8[m
+[1m--- /dev/null[m
+[1m+++ b/hola.c[m
+[36m@@ -0,0 +1,6 @@[m
+[32m+[m[32m#include <stdio.h>[m
+[32m+[m
+[32m+[m[32mint main(){[m
+[32m+[m	[32mprint("Hola mundo!");[m
+[32m+[m[32m}[m
+[32m+[m
+[1mdiff --git a/holaPython.c b/holaPython.c[m
+[1mnew file mode 100644[m
+[1mindex 0000000..cd19ac8[m
+[1m--- /dev/null[m
+[1m+++ b/holaPython.c[m
+[36m@@ -0,0 +1,23 @@[m
+[32m+[m[32m[33mcommit 4a109c9470c8d06e76ecd53ca99882c7c7db2661[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m[m
+[32m+[m[32mAuthor: Tu nombre <julicmrgo@gmail.com>[m
+[32m+[m[32mDate:   Thu Mar 25 19:31:48 2021 -0300[m
+[32m+[m
+[32m+[m[32m    Segundo py[m
+[32m+[m
+[32m+[m[32m[33mcommit 9cac4f605000b4a4180a481eb8bd3123c87cfbd7[m[33m ([m[1;31morigin/main[m[33m)[m[m
+[32m+[m[32mAuthor: Tu nombre <julicmrgo@gmail.com>[m
+[32m+[m[32mDate:   Thu Mar 25 19:22:15 2021 -0300[m
+[32m+[m
+[32m+[m[32m    Primero python[m
+[32m+[m
+[32m+[m[32m[33mcommit b9517eeb3a26373d69efb387bd0cf33a6e5be71d[m[m
+[32m+[m[32mAuthor: Tu nombre <julicmrgo@gmail.com>[m
+[32m+[m[32mDate:   Thu Mar 25 17:39:41 2021 -0300[m
+[32m+[m
+[32m+[m[32m    Cambios en el codigo[m
+[32m+[m
+[32m+[m[32m[33mcommit 378b14e6db397aa0477b36d57ab4ace9bf9ed578[m[m
+[32m+[m[32mAuthor: Tu nombre <julicmrgo@gmail.com>[m
+[32m+[m[32mDate:   Thu Mar 25 17:33:48 2021 -0300[m
+[32m+[m
+[32m+[m[32m    Primer commit[m
